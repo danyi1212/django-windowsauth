7a82f48f013a46ada7804b302bdfec7b845b7ef9
Dan <danyi1212@gmail.com>
2021-06-01 04:02:52 +0300

    Removed unnecessary checks in App.ready

--- windows_auth/apps.py
+++ windows_auth/apps.py
@@ -1,9 +1,7 @@
 import atexit
 
-from django.db import DatabaseError
 from ldap3.core.exceptions import LDAPException
 from django.apps import AppConfig
-from django.db.models import Count
 
 from windows_auth import logger
 
@@ -13,7 +11,8 @@ class WindowsAuthConfig(AppConfig):
     default_auto_field = 'django.db.models.AutoField'
 
     def ready(self):
-        from windows_auth.conf import WAUTH_IGNORE_SETTING_WARNINGS, WAUTH_PRELOAD_DOMAINS, WAUTH_DOMAINS
+        from windows_auth import checks
+        from windows_auth.conf import WAUTH_PRELOAD_DOMAINS, WAUTH_DOMAINS
         from windows_auth.settings import DEFAULT_DOMAIN_SETTING
         from windows_auth.ldap import get_ldap_manager, close_connections
 
@@ -22,19 +21,6 @@ class WindowsAuthConfig(AppConfig):
         # to be established and terminate quickly for each domain.
         # You can avoid this behavior by using "runserver --noreload" parameter,
         # or modifying the WAUTH_PRELOAD_DOMAINS setting to False.
-
-        # check about users with domain missing from settings
-        if not WAUTH_IGNORE_SETTING_WARNINGS and DEFAULT_DOMAIN_SETTING not in WAUTH_DOMAINS:
-            try:
-                from windows_auth.models import LDAPUser
-                missing_domains = LDAPUser.objects.exclude(domain__in=WAUTH_DOMAINS.keys())
-                if missing_domains.exists():
-                    for result in missing_domains.values("domain").annotate(count=Count("pk")):
-                        logger.warning(f"Settings for domain \"{result.get('domain')}\" are missing from WAUTH_DOMAINS "
-                                       f"({result.get('count')} users found)")
-            except DatabaseError as e:
-                # Table probably does not exist yet, migration is pending
-                logger.warn(e)
 
         # configure default preload domains
         preload_domains = WAUTH_PRELOAD_DOMAINS
